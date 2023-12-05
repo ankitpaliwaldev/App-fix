274550c476ebfc2aa75c594cdce0fb3344e1c1f3
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2023-12-05 13:47:09 +0700

    reformat and add send description

--- src/screens/Defoltscreen/ProductDetailesScreen/ProductDetailesScreen.js
+++ src/screens/Defoltscreen/ProductDetailesScreen/ProductDetailesScreen.js
@@ -1,20 +1,36 @@
-import React, { useState, useEffect, useMemo } from "react";
-import { Text, View, Image, ScrollView, StatusBar, Modal, FlatList, KeyboardAvoidingView, TouchableOpacity, } from "react-native";
-import { DetailsScreenStyle } from '../../../styles';
-import { SF, SH, Colors, Fooditemname, additionData } from '../../../utils';
-import { useNavigation } from '@react-navigation/native';
-import { RouteName } from '../../../routes';
-import { useSelector, useDispatch } from "react-redux";
-import { price_symbol_action } from '../../../redux/action/CommonAction';
-import { useTranslation } from 'react-i18next';
-import { useTheme } from '@react-navigation/native';
-import { VectoreIcons, SelectOptionFlatFun, IngedientsFlatList } from "../../../components";
+import React, {useState, useEffect, useMemo} from 'react';
+import {
+  Text,
+  View,
+  Image,
+  ScrollView,
+  StatusBar,
+  Modal,
+  FlatList,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+} from 'react-native';
+import {DetailsScreenStyle} from '../../../styles';
+import {SF, SH, Colors, Fooditemname, additionData} from '../../../utils';
+import {useNavigation} from '@react-navigation/native';
+import {RouteName} from '../../../routes';
+import {useSelector, useDispatch} from 'react-redux';
+import {price_symbol_action} from '../../../redux/action/CommonAction';
+import {useTranslation} from 'react-i18next';
+import {useTheme} from '@react-navigation/native';
+import {
+  VectoreIcons,
+  SelectOptionFlatFun,
+  IngedientsFlatList,
+} from '../../../components';
 
-const ProductDetailesScreen = ({ route }, props) => {
-  const { img, title, hname } = route.params;
+const ProductDetailesScreen = ({route}, props) => {
+  const {img, title, hname, description = ''} = route.params;
   const [count, setCount] = useState(1);
-  const { doctoreDetaile } = useSelector(state => state.doctorDataReducer) || { doctoreDetaile };
-  const { pricesymboldata } = useSelector(state => state.commonReducer) || {};
+  const {doctoreDetaile} = useSelector(state => state.doctorDataReducer) || {
+    doctoreDetaile,
+  };
+  const {pricesymboldata} = useSelector(state => state.commonReducer) || {};
 
   const dispatch = useDispatch();
   let PriceSymbol = '£';
@@ -24,26 +40,41 @@ const ProductDetailesScreen = ({ route }, props) => {
   }, []);
 
   const navigation = useNavigation();
-  const { t } = useTranslation();
-  const { Colors } = useTheme();
-  const DetailsScreenStyles = useMemo(() => DetailsScreenStyle(Colors), [Colors]);
+  const {t} = useTranslation();
+  const {Colors} = useTheme();
+  const DetailsScreenStyles = useMemo(
+    () => DetailsScreenStyle(Colors),
+    [Colors],
+  );
 
   const backarrow = () => {
     navigation.navigate(RouteName.HOME_TAB);
-  }
+  };
   const [modalVisibletwo, setModalVisibletwo] = useState(false);
   const [sleact, setsleact] = useState('');
   const [hearticonset, sethearticonset] = useState(0);
 
   return (
     <View style={DetailsScreenStyles.minstyleviewphotograpgy}>
-      <StatusBar barStyle="light-content" backgroundColor={'transparent'} translucent />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={'transparent'}
+        translucent
+      />
       <ScrollView
         keyboardShouldPersistTaps="handled"
         contentContainerStyle={DetailsScreenStyles.contentContainerStyle}>
         <KeyboardAvoidingView enabled>
-          <View style={[DetailsScreenStyles.minflexview, DetailsScreenStyles.bgcolorset]} >
-            <View style={[DetailsScreenStyles.minviewsigninscreen, DetailsScreenStyles.bgcolorset]}>
+          <View
+            style={[
+              DetailsScreenStyles.minflexview,
+              DetailsScreenStyles.bgcolorset,
+            ]}>
+            <View
+              style={[
+                DetailsScreenStyles.minviewsigninscreen,
+                DetailsScreenStyles.bgcolorset,
+              ]}>
               {/* <TouchableOpacity style={DetailsScreenStyles.flrxfireiocnview} onPress={() => navigation.navigate(RouteName.RATING_SCREEN_SET)}>
                 <TouchableOpacity style={DetailsScreenStyles.setdotflex}>
                   <Text style={DetailsScreenStyles.caltextstyle}>4.5</Text>
@@ -53,14 +84,25 @@ const ProductDetailesScreen = ({ route }, props) => {
               <View style={DetailsScreenStyles.setimagestylewidth}>
                 <Image style={DetailsScreenStyles.imagsetstyle} source={img} />
               </View>
-              <TouchableOpacity onPress={() => backarrow()} style={DetailsScreenStyles.settextstyle}>
+              <TouchableOpacity
+                onPress={() => backarrow()}
+                style={DetailsScreenStyles.settextstyle}>
                 <View style={DetailsScreenStyles.setbgcolorviewtwoview}>
                   <Text style={DetailsScreenStyles.textstyle}>
-                    <VectoreIcons icon="Feather" name="chevrons-left" size={SF(27)} color={Colors.white_text_color} />
+                    <VectoreIcons
+                      icon="Feather"
+                      name="chevrons-left"
+                      size={SF(27)}
+                      color={Colors.white_text_color}
+                    />
                   </Text>
                 </View>
               </TouchableOpacity>
-              <View style={[DetailsScreenStyles.bgwhiteboxminview, DetailsScreenStyles.bgcolorset]} >
+              <View
+                style={[
+                  DetailsScreenStyles.bgwhiteboxminview,
+                  DetailsScreenStyles.bgcolorset,
+                ]}>
                 {/* <View style={DetailsScreenStyles.bgwhiteboxsmall}>
                   <Text style={DetailsScreenStyles.settextweight}>{t(title)}</Text>
                 </View> */}
@@ -74,45 +116,89 @@ const ProductDetailesScreen = ({ route }, props) => {
                   </TouchableOpacity>
                 } */}
 
-                <View style={[DetailsScreenStyles.setallpading, DetailsScreenStyles.bgcolorset]}>
-                  <View style={[DetailsScreenStyles.descripitionviewone, DetailsScreenStyles.bgcolorset]}>
-                    <Text style={DetailsScreenStyles.descriptiontextset}>{"Product Description"}</Text>
-                    <Text style={DetailsScreenStyles.paregraphtextset}>{t("Discription_Item_Label")}</Text>
-
+                <View
+                  style={[
+                    DetailsScreenStyles.setallpading,
+                    DetailsScreenStyles.bgcolorset,
+                  ]}>
+                  <View
+                    style={[
+                      DetailsScreenStyles.descripitionviewone,
+                      DetailsScreenStyles.bgcolorset,
+                    ]}>
+                    <Text style={DetailsScreenStyles.descriptiontextset}>
+                      {'Product Description'}
+                    </Text>
+                    <Text style={DetailsScreenStyles.paregraphtextset}>
+                      {/* {t('Discription_Item_Label')} */}
+                      {description}
+                    </Text>
                   </View>
-                  <View style={[DetailsScreenStyles.descripitionviewtwo, DetailsScreenStyles.bgcolorset]}>
-                    <View style={DetailsScreenStyles.borderbottomview}>
-                      <Text style={DetailsScreenStyles.nuteialtext}>{"Additional Info:"}</Text>
+                  <View
+                    style={[
+                      DetailsScreenStyles.descripitionviewtwo,
+                      DetailsScreenStyles.bgcolorset,
+                    ]}>
+                    <View style={[DetailsScreenStyles.borderbottomview]}>
+                      <Text style={DetailsScreenStyles.nuteialtext}>
+                        {'Additional Info:'}
+                      </Text>
                       <View style={DetailsScreenStyles.flextextset}>
-                        <Text style={DetailsScreenStyles.proteintextset}>{t("Protein_Label")}</Text>
-                        <Text style={DetailsScreenStyles.setdigitaltextview}>2.5{t("gLabel")}</Text>
+                        <Text style={DetailsScreenStyles.proteintextset}>
+                          {t('Protein_Label')}
+                        </Text>
+                        <Text style={DetailsScreenStyles.setdigitaltextview}>
+                          2.5{t('gLabel')}
+                        </Text>
                       </View>
                       <View style={DetailsScreenStyles.flextextset}>
-                        <Text style={DetailsScreenStyles.proteintextset}>{t("Carbohydrates_Label")}</Text>
-                        <Text style={DetailsScreenStyles.setdigitaltextview}>14.7{t("gLabel")}</Text>
+                        <Text style={DetailsScreenStyles.proteintextset}>
+                          {t('Carbohydrates_Label')}
+                        </Text>
+                        <Text style={DetailsScreenStyles.setdigitaltextview}>
+                          14.7{t('gLabel')}
+                        </Text>
                       </View>
                       <View style={DetailsScreenStyles.flextextset}>
-                        <Text style={DetailsScreenStyles.proteintextset}>{t("Sodium_Label")}</Text>
-                        <Text style={DetailsScreenStyles.setdigitaltextview}>19%*</Text>
+                        <Text style={DetailsScreenStyles.proteintextset}>
+                          {t('Sodium_Label')}
+                        </Text>
+                        <Text style={DetailsScreenStyles.setdigitaltextview}>
+                          19%*
+                        </Text>
                       </View>
                       <View style={DetailsScreenStyles.flextextset}>
-                        <Text style={DetailsScreenStyles.proteintextset}>{t("Potassium_Label")}</Text>
-                        <Text style={DetailsScreenStyles.setdigitaltextview}>5%*</Text>
+                        <Text style={DetailsScreenStyles.proteintextset}>
+                          {t('Potassium_Label')}
+                        </Text>
+                        <Text style={DetailsScreenStyles.setdigitaltextview}>
+                          5%*
+                        </Text>
                       </View>
-                      <Text style={DetailsScreenStyles.proteintextset}>{t("Rich_in_Vitamin_Label")}</Text>
+                      <Text style={DetailsScreenStyles.proteintextset}>
+                        {t('Rich_in_Vitamin_Label')}
+                      </Text>
                     </View>
                     <View style={DetailsScreenStyles.thretexztflexviewtwo}>
                       <View style={DetailsScreenStyles.flexclockandtextwo}>
-                        < VectoreIcons icon="MaterialIcons" name="local-fire-department" color={Colors.bittersweet_color} size={SF(14)} />
-                        <Text style={DetailsScreenStyles.textcolorgray}>{t("Food_Item_Cal_Label")}</Text>
+                        <VectoreIcons
+                          icon="MaterialIcons"
+                          name="local-fire-department"
+                          color={Colors.bittersweet_color}
+                          size={SF(14)}
+                        />
+                        <Text style={DetailsScreenStyles.textcolorgray}>
+                          {t('Food_Item_Cal_Label')}
+                        </Text>
                       </View>
                       <View>
-                        <Text style={DetailsScreenStyles.textcolorgraytwo}>{t("Daily_value_Label")}</Text>
+                        <Text style={DetailsScreenStyles.textcolorgraytwo}>
+                          {t('Daily_value_Label')}
+                        </Text>
                       </View>
                     </View>
                   </View>
-                  <View>
-                  </View>
+                  <View />
                 </View>
                 {/* <View style={[DetailsScreenStyles.setwidthbox, DetailsScreenStyles.bgcolorset]}>
                   <View style={DetailsScreenStyles.setboxshadowimage}>
@@ -165,28 +251,51 @@ const ProductDetailesScreen = ({ route }, props) => {
                   </Modal>
                 </View> */}
               </View>
-              <View>
-              </View>
+              <View />
             </View>
           </View>
         </KeyboardAvoidingView>
       </ScrollView>
       <View style={DetailsScreenStyles.setbgcolorviewtwo}>
         <TouchableOpacity style={DetailsScreenStyles.setwidthprice}>
-          <Text style={DetailsScreenStyles.pricetextsetviewtwo}>{pricesymboldata} {125 * count}</Text>
+          <Text style={DetailsScreenStyles.pricetextsetviewtwo}>
+            {pricesymboldata} {125 * count}
+          </Text>
         </TouchableOpacity>
         <View style={DetailsScreenStyles.plusandminusflexview}>
-          <TouchableOpacity onPress={() => { if (count > 1) { setCount(count - 1) } }}>
-            <VectoreIcons icon="Entypo" name="minus" size={SF(20)} color={Colors.white_text_color} />
+          <TouchableOpacity
+            onPress={() => {
+              if (count > 1) {
+                setCount(count - 1);
+              }
+            }}>
+            <VectoreIcons
+              icon="Entypo"
+              name="minus"
+              size={SF(20)}
+              color={Colors.white_text_color}
+            />
           </TouchableOpacity>
           <Text style={DetailsScreenStyles.pricetextsetview}>{count}</Text>
           <TouchableOpacity onPress={() => setCount(count + 1)}>
-            <VectoreIcons icon="Entypo" name="plus" size={SF(20)} color={Colors.white_text_color} />
+            <VectoreIcons
+              icon="Entypo"
+              name="plus"
+              size={SF(20)}
+              color={Colors.white_text_color}
+            />
           </TouchableOpacity>
         </View>
         <View>
-          <TouchableOpacity style={DetailsScreenStyles.setbgcolorwhite} onPress={() => navigation.navigate(RouteName.CART_TAB)}>
-            <VectoreIcons icon="Ionicons" name="cart" color={Colors.black_text_color} size={SF(24)} />
+          <TouchableOpacity
+            style={DetailsScreenStyles.setbgcolorwhite}
+            onPress={() => navigation.navigate(RouteName.CART_TAB)}>
+            <VectoreIcons
+              icon="Ionicons"
+              name="cart"
+              color={Colors.black_text_color}
+              size={SF(24)}
+            />
           </TouchableOpacity>
         </View>
       </View>
--- src/screens/Home/Tab/HomeTab.js
+++ src/screens/Home/Tab/HomeTab.js
@@ -1,62 +1,70 @@
-import React, { useEffect, useState, useMemo } from 'react';
-import { View, ScrollView, Text, FlatList, StatusBar } from "react-native";
-import { HometabStyle } from '../../../styles';
-import { RouteName } from '../../../routes';
-import { useDispatch } from "react-redux";
-import { TrendingHomeFlatFun, HomeFirstImageSlider, HomeCategoryFlatFun, DealsOfTheDay, PopularHomeFlatFun, Spacing } from '../../../components';
-import { get_doctore_category_action } from '../../../redux/action/DoctoreCategoryAction';
-import { useSelector } from "react-redux";
-import { get_doctore_detailes_action } from '../../../redux/action/DoctoreDataAction';
-import { SearchHeaderScreen } from '../../../screens';
-import { useTranslation } from 'react-i18next';
-import { useTheme } from '@react-navigation/native';
-import { price_symbol_action } from '../../../redux/action/CommonAction';
-import { SH, MedicineCategoryHomeTab, MegaMedicine } from '../../../utils';
-import TodayDeals from "../../../../data.json"
+import React, {useEffect, useState, useMemo} from 'react';
+import {View, ScrollView, Text, FlatList, StatusBar} from 'react-native';
+import {HometabStyle} from '../../../styles';
+import {RouteName} from '../../../routes';
+import {useDispatch} from 'react-redux';
+import {
+  TrendingHomeFlatFun,
+  HomeFirstImageSlider,
+  HomeCategoryFlatFun,
+  DealsOfTheDay,
+  PopularHomeFlatFun,
+  Spacing,
+} from '../../../components';
+import {get_doctore_category_action} from '../../../redux/action/DoctoreCategoryAction';
+import {useSelector} from 'react-redux';
+import {get_doctore_detailes_action} from '../../../redux/action/DoctoreDataAction';
+import {SearchHeaderScreen} from '../../../screens';
+import {useTranslation} from 'react-i18next';
+import {useTheme} from '@react-navigation/native';
+import {price_symbol_action} from '../../../redux/action/CommonAction';
+import {SH, MedicineCategoryHomeTab, MegaMedicine} from '../../../utils';
+import TodayDeals from '../../../../data.json';
 import {add_cart} from '../../../redux/action/cartAction';
 
-const HomeTabset = (props) => {
-  const { navigation } = props;
+const HomeTabset = props => {
+  const {navigation} = props;
   const dispatch = useDispatch();
-  const { pricesymboldata } = useSelector(state => state.commonReducer) || {};
+  const {pricesymboldata} = useSelector(state => state.commonReducer) || {};
   const [liked, setLiked] = useState([]);
-  const { t } = useTranslation();
-  const { Colors } = useTheme();
+  const {t} = useTranslation();
+  const {Colors} = useTheme();
   const HometabStyles = useMemo(() => HometabStyle(Colors), [Colors]);
-  const {cartData} = useSelector(state => state.cartInfo)
+  const {cartData} = useSelector(state => state.cartInfo);
 
   let PriceSymbol = '£';
   useEffect(() => {
-    dispatch(price_symbol_action(PriceSymbol))
+    dispatch(price_symbol_action(PriceSymbol));
   }, []);
 
-  const doctordata = (docterdata) => {
+  const doctordata = docterdata => {
     dispatch(add_cart([...cartData, docterdata]));
-    dispatch(get_doctore_detailes_action(docterdata))
-    navigation.navigate(RouteName.CART_TAB)
-  }
+    dispatch(get_doctore_detailes_action(docterdata));
+    navigation.navigate(RouteName.CART_TAB);
+  };
 
-  const doctordatatendingmenu = (docterdata) => {
+  const doctordatatendingmenu = docterdata => {
     dispatch(add_cart([...cartData, docterdata]));
-    dispatch(get_doctore_detailes_action(docterdata))
-    navigation.navigate(RouteName.CART_TAB)
-  }
-  const LikeUnlikeFun = (index) => {
+    dispatch(get_doctore_detailes_action(docterdata));
+    navigation.navigate(RouteName.CART_TAB);
+  };
+  const LikeUnlikeFun = index => {
     if (liked.includes(index)) {
-      let unlike = liked.filter((elem) => elem !== index);
+      let unlike = liked.filter(elem => elem !== index);
       setLiked(unlike);
     } else {
       setLiked([...liked, index]);
     }
-  }
+  };
 
-  const doctordatacategoryes = (doctordatacategoryes) => {
-    dispatch(get_doctore_category_action(doctordatacategoryes))
-    navigation.navigate(RouteName.PRODUCT_LIST_ITEM)
-  }
+  const doctordatacategoryes = doctordatacategoryes => {
+    dispatch(get_doctore_category_action(doctordatacategoryes));
+    navigation.navigate(RouteName.PRODUCT_LIST_ITEM);
+  };
 
   return (
-    <View style={[HometabStyles.minstyleviewphotograpgy, HometabStyles.bgcolorset]}>
+    <View
+      style={[HometabStyles.minstyleviewphotograpgy, HometabStyles.bgcolorset]}>
       <StatusBar backgroundColor={Colors.bgwhite} barStyle={'dark-content'} />
 
       <ScrollView
@@ -64,48 +72,84 @@ const HomeTabset = (props) => {
         contentContainerStyle={HometabStyles.contentContainerStyle}>
         <View style={HometabStyles.minflexview}>
           {/* componets import */}
-          <SearchHeaderScreen onPress={() => navigation.navigate(RouteName.POPULAR_SCREEN)} />
-          <HomeFirstImageSlider onPress={() => navigation.navigate(RouteName.PRODUCT_LIST_ITEM)} />
+          <SearchHeaderScreen
+            onPress={() => navigation.navigate(RouteName.POPULAR_SCREEN)}
+          />
+          <HomeFirstImageSlider
+            onPress={() => navigation.navigate(RouteName.PRODUCT_LIST_ITEM)}
+          />
           {/* componets end */}
           <View style={HometabStyles.marginsetminview}>
-            <Text style={HometabStyles.settopcategoriesnine}>{t("Trending_Categories_Label")}</Text>
+            <Text style={HometabStyles.settopcategoriesnine}>
+              {t('Trending_Categories_Label')}
+            </Text>
             <FlatList
               data={MedicineCategoryHomeTab}
-              renderItem={({ item, index }) => (<HomeCategoryFlatFun item={item} onPress={() => doctordatacategoryes(item)} />)}
+              renderItem={({item, index}) => (
+                <HomeCategoryFlatFun
+                  item={item}
+                  onPress={() => doctordatacategoryes(item)}
+                />
+              )}
               keyExtractor={item => item.id}
               horizontal
               showsHorizontalScrollIndicator={false}
               style={HometabStyles.setflex}
               contentContainerStyle={HometabStyles.categoryContainer}
             />
-            <View style={[HometabStyles.settopmargin, HometabStyles.bgcolorset]}>
-              <Text style={HometabStyles.settopcategories}>{t("Deals_Of_The_Day_Label")}</Text>
+            <View
+              style={[HometabStyles.settopmargin, HometabStyles.bgcolorset]}>
+              <Text style={HometabStyles.settopcategories}>
+                {t('Deals_Of_The_Day_Label')}
+              </Text>
               <FlatList
                 data={TodayDeals}
-                renderItem={({ item, index }) => (
-                  <DealsOfTheDay key={index} item={item}
-                    onPress={() => navigation.navigate(RouteName.PRODUCT_DETAILS_SCREEN, { img: item.image, sku: item.SKU, name: item.name })}
+                renderItem={({item, index}) => (
+                  <DealsOfTheDay
+                    key={index}
+                    item={item}
+                    onPress={() =>
+                      navigation.navigate(RouteName.PRODUCT_DETAILS_SCREEN, {
+                        img: item.image,
+                        sku: item.SKU,
+                        name: item.name,
+                        description: item.description,
+                      })
+                    }
                     DoctorHandle={() => doctordata(item)}
                     pricesymboldata={pricesymboldata}
-                  />)}
+                  />
+                )}
                 keyExtractor={item => item.id}
                 horizontal={true}
                 showsHorizontalScrollIndicator={false}
                 contentContainerStyle={HometabStyles.dealsContainer}
               />
             </View>
-            <Text style={HometabStyles.settopcategories}>{t("Popular_Medicine_Label")}</Text>
+            <Text style={HometabStyles.settopcategories}>
+              {t('Popular_Medicine_Label')}
+            </Text>
             <FlatList
               data={TodayDeals}
-              renderItem={({ item, index }) => (
-                <PopularHomeFlatFun key={index} item={item}
-                  onPress={() => navigation.navigate(RouteName.PRODUCT_DETAILS_SCREEN, { img: item.image, sku: item.SKU, name: item.name })}
+              renderItem={({item, index}) => (
+                <PopularHomeFlatFun
+                  key={index}
+                  item={item}
+                  onPress={() =>
+                    navigation.navigate(RouteName.PRODUCT_DETAILS_SCREEN, {
+                      img: item.image,
+                      sku: item.SKU,
+                      name: item.name,
+                      description: item.description,
+                    })
+                  }
                   CartTabHandle={() => {
                     dispatch(add_cart([...cartData, item]));
                     navigation.navigate(RouteName.CART_TAB);
                   }}
                   pricesymboldata={pricesymboldata}
-                />)}
+                />
+              )}
               keyExtractor={item => item.id}
               horizontal={true}
               showsHorizontalScrollIndicator={false}
@@ -114,18 +158,29 @@ const HomeTabset = (props) => {
             />
           </View>
           <Spacing />
-          <Text style={HometabStyles.settopcategories}>{t("Trending_Store_Label")}</Text>
+          <Text style={HometabStyles.settopcategories}>
+            {t('Trending_Store_Label')}
+          </Text>
           <Spacing space={SH(5)} />
           <FlatList
             data={MegaMedicine}
-            renderItem={({ item, index }) => (
-              <TrendingHomeFlatFun item={item} index={index}
-                LikeUnlikeFun={(index) => LikeUnlikeFun(index)}
-                onPress={() => navigation.navigate((RouteName.PRODUCT_DETAILS_SCREEN), { img: item.image, title: item.text, hname: item.hospitalname })}
+            renderItem={({item, index}) => (
+              <TrendingHomeFlatFun
+                item={item}
+                index={index}
+                LikeUnlikeFun={index => LikeUnlikeFun(index)}
+                onPress={() =>
+                  navigation.navigate(RouteName.PRODUCT_DETAILS_SCREEN, {
+                    img: item.image,
+                    title: item.text,
+                    hname: item.hospitalname,
+                  })
+                }
                 doctordatatendingmenu={() => doctordatatendingmenu(item)}
                 pricesymboldata={pricesymboldata}
                 liked={liked}
-              />)}
+              />
+            )}
             keyExtractor={item => item.id}
             horizontal={true}
             showsHorizontalScrollIndicator={false}
